def4369333de0c8e7b154a87a986414b30b5abe4
marciotamega-rgb <marciotamega@gmail.com>
2026-08-04 22:47:19 -0300

    Atualizacao app
    mexi no global.css, no reset.css, main.jsx e app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import "./css/reset.css"
+import "./css/global.css"
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+createRoot(document.getElementById('root')).
+<StrictMode>
+  <App />
   </StrictMode>,
-)
+  )
